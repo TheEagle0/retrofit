f43ac7a3618926e850f14edb858557fafd8486e8
ahmedkamar10@gmail.com <meandmyswordareinvincible>
2018-12-10 18:59:18 +0200

    Zeft

--- app/src/main/java/com/example/theeagle/appexample/ui/ViewModel.kt
+++ app/src/main/java/com/example/theeagle/appexample/ui/ViewModel.kt
@@ -3,14 +3,19 @@ package com.example.theeagle.appexample.ui
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.MutableLiveData
+import arrow.core.Option
 import com.example.theeagle.appexample.entity.Result
 import com.example.theeagle.appexample.ritrofit.RequestData
+import kotlinx.coroutines.*
 
 
 class ViewModel(app: Application) : AndroidViewModel(app) {
 
-    private val retrievedDataObservable = RequestData.getDataWithRetrofit()
+
+    @ExperimentalCoroutinesApi
     fun getDataObservable(): MutableLiveData<Result> {
+        val retrievedDataObservable = runBlocking { withContext(Dispatchers.Default) { RequestData.getDataWithRetrofit() } }
+
         return retrievedDataObservable
     }
 }
