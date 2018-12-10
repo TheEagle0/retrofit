d3fcb956dc621c0a8dfe8f8b14b367c32d3d9720
ahmedkamar10@gmail.com <meandmyswordareinvincible>
2018-12-10 10:30:33 +0200

    Add Ui

--- app/src/main/java/com/example/theeagle/appexample/entity/DataEntity.kt
+++ app/src/main/java/com/example/theeagle/appexample/entity/DataEntity.kt
@@ -4,8 +4,9 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 
-data class DataEntity(@field:SerializedName("sectionName") @Expose val articleName: String, @field:SerializedName("webTitle") @Expose val section: String)
+data class DataEntity(@field:SerializedName("sectionName") @Expose val articleName: String,
+                      @field:SerializedName("webTitle") @Expose val section: String)
 
 data class DataList(@field:SerializedName("results") @Expose var articleList: MutableList<DataEntity>)
 
-data class OuterObject(@field:SerializedName("response") @Expose val bigObject:DataList)
+data class OuterObject(@field:SerializedName("response") @Expose val bigObject: DataList)
